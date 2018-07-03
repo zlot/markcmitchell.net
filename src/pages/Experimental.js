37e38f749bd9948cee7f19d2671227e4c847244c
Mitchell, Mark <mark.mitchell@fjordnet.com>
2018-07-03 22:56:24 +1000

    Update Experimental coming soon style

--- src/pages/Experimental.js
+++ src/pages/Experimental.js
@@ -2,9 +2,13 @@ import React from 'react'
 
 export default class Experimental extends React.Component {
   render = () => (
-    <div>
-      <h1>Experimental</h1>
-      <h2>Coming soon 🏝</h2>
+    <div className='Experimental container'>
+      <div className='row'>
+        <div className='col-12'>
+          <h1>Experimental</h1>
+          <p>Coming soon 🏝</p>
+        </div>
+      </div>
     </div>
   )
 }
